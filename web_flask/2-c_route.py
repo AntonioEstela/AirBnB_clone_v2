74f80839afcf0d63b3a8ce2309fc0a65b8550cfb
Antonio Estela <antonioestela73@gmail.com>
2020-09-01 16:04:19 -0500

    Added task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Return some stuff"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return some stuff"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Return some stuff"""
+    txt = text.replace('_', ' ')
+    return 'C {}'.format(txt)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
