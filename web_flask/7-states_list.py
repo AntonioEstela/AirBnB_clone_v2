b440b83e9e1d00abeffd1651da582c10e71495d0
Antonio Estela <antonioestela73@gmail.com>
2020-09-02 10:46:45 -0500

    Added python file for task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, render_template, g
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display all states in the database"""
+    data = storage.all(State).values()
+    return render_template('7-states_list.html', states=data)
+
+
+@app.teardown_appcontext
+def db_close(self):
+    """Method to close the session after each request"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
